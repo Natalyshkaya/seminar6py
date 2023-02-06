b6a29c19c5c042ec0dc4c1f8b16eabc5bc83b43d
Nataly <naty.sh@mail.ru>
2023-02-06 23:36:37 +0500

    задача 1

---
+++ задача1/zadacha1.py
@@ -0,0 +1,11 @@
+# Пользователь вводит число K - количество фруктов. Затем он вводит K фруктов в формате: название фрукта и его количество. 
+# Добавьте все фрукты в словарь, где название фрукта - это ключ, а количество - значение
+
+print('введи количество')
+count = int(input())
+fruit_dict = {}
+for _ in range(count):
+    fruit = input()
+    count_fruits = int(input())
+    fruit_dict[fruit] = count_fruits
+print(fruit_dict)
